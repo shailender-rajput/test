ca0fbaaad63b1a4e070a907ef85199c29f362918
shailender-rajput <shailender.mcs.du.2012@gmail.com>
2015-06-05 14:11:47 +0530

    project addition
    new project for testing git repository

---
+++ MyService/MyServiceWebApplication/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MyServiceWebApplication.MvcApplication" Language="C#" %>
---
+++ MyService/MyServiceWebApplication/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MyServiceWebApplication.Startup))]
+namespace MyServiceWebApplication
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
